cf238578495f4a18b3eff19d9b64af87155882b7
Yadav106 <sahil.jp.yadav@gmail.com>
2023-09-12 17:34:14 +0530

    worked on my profile

--- app/pages/myprofile/page.tsx
+++ app/pages/myprofile/page.tsx
@@ -1,7 +1,13 @@
-const MyProfile = () => {
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import Image from "next/image";
+
+const MyProfile = async () => {
+    const currentUser = await getCurrentUser();
     return ( 
-        <div className="mt-[200px]">
-            This is User Profile
+        <div className="mt-[200px] flex flex-col items-center">
+            <Image alt="Avatar" className=" rounded-full" src={currentUser?.image || '/images/placeholder.jpg'} height={100} width={100}/>
+            <div className="text-2xl font-semibold mt-5">{currentUser?.name}</div>
+            <div className="text-xl font-semibold mt-5">{currentUser?.email}</div>
         </div>
      );
 }
