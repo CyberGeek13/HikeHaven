2d7eeb7ef5864d5bc12b95979aac1b28e26b8a01
Yadav106 <sahil.jp.yadav@gmail.com>
2023-09-17 11:15:40 +0530

    team updated

--- app/hooks/useTeam.tsx
+++ app/hooks/useTeam.tsx
@@ -1,4 +1,7 @@
 import { useMemo } from "react";
+import { AiFillLinkedin, AiFillInstagram } from "react-icons/ai"
+import { BsFacebook } from "react-icons/bs"
+import { MdEmail } from "react-icons/md"
 
 const useTeam = () => {
     const team = useMemo(() => [
@@ -7,14 +10,20 @@ const useTeam = () => {
             position: "CEO",
             image: "/images/team/purav.jpg",
             quote: "Do your job wisely and don't forget to smile.",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id rhoncus luctus, nisl libero ultrices urna, eget lacinia nisl velit eget mauris. Sed euismod, diam id rhoncus luctus, nisl libero ultrices urna, eget lacinia nisl velit eget mauris."
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id rhoncus luctus, nisl libero ultrices urna, eget lacinia nisl velit eget mauris. Sed euismod, diam id rhoncus luctus, nisl libero ultrices urna, eget lacinia nisl velit eget mauris.",
+            linkedin: "https://www.linkedin.com/in/purav-ajmera-2602a51aa",
+            instagram: "https://instagram.com/purav.13?igshid=OGQ5ZDc2ODk2ZA==",
+            facebook: "https://www.facebook.com/purav.ajmera?mibextid=b06tZ0",
+            email: "mailto:puravajmera2@gmail.com"
         },
         {
             name: "Sahil Yadav",
             position: "Manager",
             image: "/images/team/yadavji.jpg",
             quote: "Monkey D. Luffy is the future Pirate King.",
-            description: "Jhingalala Hoo Hoo"
+            description: "Jhingalala Hoo Hoo",
+            linkedin: "https://www.linkedin.com/in/sahil-yadav106/",
+            email: "mailto:sahiljyadav12345@gmail.com"
         }
     ], [])
 
--- app/pages/team/components/Member.tsx
+++ app/pages/team/components/Member.tsx
@@ -1,4 +1,8 @@
+'use client';
+
 import Image from "next/image";
+import { AiFillLinkedin, AiFillInstagram, AiFillFacebook } from "react-icons/ai"
+import { MdEmail } from "react-icons/md"
 
 interface MemberProps {
     name: string;
@@ -6,6 +10,10 @@ interface MemberProps {
     image: string;
     quote: string;
     description: string;
+    linkedin?: string;
+    instagram?: string;
+    facebook?: string;
+    email?: string;
 }
 
 const Member: React.FC<MemberProps> = ({
@@ -13,7 +21,11 @@ const Member: React.FC<MemberProps> = ({
     position,
     image,
     quote,
-    description
+    description,
+    linkedin,
+    instagram,
+    facebook,
+    email
 }) => {
     return ( 
         <div className="w-[300px] p-[30px] rounded-md bg-gray-100 shadow-lg hover:shadow-2xl transition duration-200 flex flex-col justify-center items-center gap-[20px]">
@@ -26,6 +38,40 @@ const Member: React.FC<MemberProps> = ({
             </div>
             <div>
                 {quote}
+            </div>
+            <div className="flex flex-wrap gap-[10px] justify-center items-center">
+                {
+                    linkedin && (
+                        <AiFillLinkedin 
+                            className="text-[30px] cursor-pointer hover:text-blue-500 transition duration-200"
+                            onClick={() => window.open(linkedin, "_blank")}
+                        />
+                    )
+                }
+                {
+                    instagram && (
+                        <AiFillInstagram 
+                            className="text-[30px] cursor-pointer hover:text-pink-500 transition duration-200"
+                            onClick={() => window.open(instagram, "_blank")}
+                        />
+                    )
+                }
+                {
+                    facebook && (
+                        <AiFillFacebook 
+                            className="text-[30px] cursor-pointer hover:text-blue-500 transition duration-200"
+                            onClick={() => window.open(facebook, "_blank")}
+                        />
+                    )
+                }
+                {
+                    email && (
+                        <MdEmail 
+                            className="text-[30px] cursor-pointer hover:text-red-500 transition duration-200"
+                            onClick={() => window.open(email, "_blank")}
+                        />
+                    )
+                }
             </div>
         </div>
      );
